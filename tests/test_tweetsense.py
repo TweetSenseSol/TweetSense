0e4bb8b6e24a72bb67f4555d711a055e54e826f5
TweetSenseAI <tweetsensesol@gmail.com>
2025-01-12 21:00:14 +0100

    Update test_tweetsense.py

--- tests/test_tweetsense.py
+++ tests/test_tweetsense.py
@@ -1,1 +1,19 @@
 
+import unittest
+from tweetsense.sentiment_analysis import analyze_sentiment, label_sentiment
+
+class TestTweetSense(unittest.TestCase):
+    def test_analyze_sentiment(self):
+        text = "I love using this new product!"
+        polarity, subjectivity = analyze_sentiment(text)
+        self.assertGreater(polarity, 0)
+        self.assertGreaterEqual(subjectivity, 0)
+
+    def test_label_sentiment(self):
+        self.assertEqual(label_sentiment(0.5), "positive")
+        self.assertEqual(label_sentiment(-0.5), "negative")
+        self.assertEqual(label_sentiment(0), "neutral")
+
+if __name__ == '__main__':
+    unittest.main()
+
